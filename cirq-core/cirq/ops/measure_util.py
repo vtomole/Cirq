62f9cefee5b91aaf0d1b4c897506192f155d5937
joe <joesho112358@gmail.com>
2021-07-12 18:18:17 -0400

    updating code style and types for measure_util

--- cirq-core/cirq/ops/measure_util.py
+++ cirq-core/cirq/ops/measure_util.py
@@ -111,7 +111,7 @@ def _get_each_measurement(
 
 def _get_measurement(
     target: Union['cirq.Qid', Iterable['cirq.Qid']],
-    key: [Union[str, value.MeasurementKey]],
+    key: Optional[Union[str, value.MeasurementKey]] = None,
     invert_mask: Tuple[bool, ...] = (),
 ) -> Union[raw_types.Operation, List[raw_types.Operation]]:
     if key is None:
@@ -121,8 +121,7 @@ def _get_measurement(
 
 
 def _get_each_measurement(
-        target: Iterable['cirq.Qid'],
-        key_func: Callable[[raw_types.Qid], str]
+    target: Iterable['cirq.Qid'], key_func: Callable[[raw_types.Qid], str]
 ) -> List[raw_types.Operation]:
     return [MeasurementGate(1, key_func(q), qid_shape=(q.dimension,)).on(q) for q in target]
 
@@ -167,8 +166,7 @@ def measure(
 
 
 def measure_each(
-    *qubits: Union['cirq.Qid', Iterable['cirq.Qid']],
-    key_func: Callable[[raw_types.Qid], str] = str
+    *qubits: Union['cirq.Qid', Iterable['cirq.Qid']], key_func: Callable[[raw_types.Qid], str] = str
 ) -> List[raw_types.Operation]:
     """Returns a list of operations individually measuring the given qubits.
 
